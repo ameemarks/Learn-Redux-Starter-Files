5893dc1655543e9d8814add8ef2431de611d7da1
Anna Grochola <agrochola@cyfrowypolsat.pl>
2018-08-24 18:01:22 +0200

    Updating comment state

--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -1,19 +1,22 @@
 import React from 'react';
-import Photo from './Photo'
+import Photo from './Photo';
+import Comments from './Comments';
 
 const Single = React.createClass({
     render() {
+        const { postId } = this.props.params;
         // index of the post
         const index = this.props.posts.findIndex((post) => {
-            return post.code === this.props.params.postId;
+            return post.code === postId;
         });
-        const post = this.props.posts[index];
-        console.log(index);
-        console.log(post);
         // get the post
+        const post = this.props.posts[index];
+        // get the comments for the proper post (or an empty arr when no comments)
+        const postComments = this.props.comments[postId] || [];
         return (
             <div className="single-photo">
                 <Photo index={index} post={post} {...this.props}/>
+                <Comments postComments={postComments}/>
             </div>
         )
     }
